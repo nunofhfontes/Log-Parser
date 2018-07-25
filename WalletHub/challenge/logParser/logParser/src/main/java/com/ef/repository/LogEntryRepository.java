a2661d0c2b8c1c8518e1b4c5fc47fd5f4502d8a0
unknown <nunof.h.fontes@gmail.com>
2018-07-25 20:23:39 +0100

    jpa repository updated

--- WalletHub/challenge/logParser/logParser/src/main/java/com/ef/repository/LogEntryRepository.java
+++ WalletHub/challenge/logParser/logParser/src/main/java/com/ef/repository/LogEntryRepository.java
@@ -1,10 +1,20 @@
 package com.ef.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ef.domain.LogEntry;
 
 public interface LogEntryRepository extends JpaRepository<LogEntry, Long>{
 
+	public Optional<LogEntry> findById(Long id);
+	
+	public List<LogEntry> findAll();
+	
+	public List<LogEntry> findFirst10ByOrderByInsertionDateDesc();
+	
+	
 }
 
